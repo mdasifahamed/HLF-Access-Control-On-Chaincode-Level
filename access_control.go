082543c97f26ba9ced4ed0399862aa3e855efafa
mdasifahamed <mdasifamahed89@yahoo.com>
2024-05-20 11:32:55 +0600

    Definations

--- access_control.go
+++ access_control.go
@@ -7,46 +7,65 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
+// Chaincode Struct Which Is The Main Chaincode On Which All The Functinalties Will Be Applied
 type TestContract struct {
 	contractapi.Contract
 }
 
-type Asset struct {
+type Musics struct {
 	ID    string `json:ID`
 	Name  string `json:Name`
 	Owner string `json:Owner`
 }
 
-func (contract *Asset) InitAssets(ctx contractapi.TransactionContextInterface) (string, error) {
+func (contract *Musics) InitAssets(ctx contractapi.TransactionContextInterface) (string, error) {
+	/*
+		fucn InitAssets() is method of the Asset Struct
+		Usage: It Adds Dummy  Assets To The Legder
+		@param: 'ctx'  is type of 'contractapi.TransactionContextInterface' Which Provide all the
+		ShimAPI Methods To Interact Within The Networks
 
-	assets := []Asset{
+		returns: A String If The Transaction is SuccessFull or it will be if the the function fails it will return an error message
+
+
+	*/
+
+	// dummy asset
+	assets := []Musics{
 		{
 			ID:    "1",
-			Name:  "Land",
-			Owner: "Jack",
+			Name:  "Shiloutes",
+			Owner: "Avicii",
 		},
 
 		{
 			ID:    "2",
-			Name:  "Car",
-			Owner: "Patrick",
+			Name:  "Levels",
+			Owner: "Avicii",
 		},
 
 		{
 			ID:    "3",
-			Name:  "Yacht",
-			Owner: "Patrick",
+			Name:  "Sunshine",
+			Owner: "Avicii",
 		},
 	}
-
+	// Iteration over the assets array
+	// to retrive every asset
 	for _, asset := range assets {
 
-		jsonAsset, err := json.Marshal(asset)
+		jsonAsset, err := json.Marshal(asset) // Converting the asset struct to the json
 
 		if err != nil {
-			return "", fmt.Errorf("Failed To Convert The Asset To Json")
+			return "", fmt.Errorf("Failed To Convert The Asset To Json") // returns error if the it fails to convert i to the array
 		}
 
+		// it calls the shimpapi method over getstub() method to putstate()
+		// to add the asset to ledger
+		// PutState() method retquires two params
+		// one is the key and its type must be a string
+		// it is the key by which we can query over the ledger for an asset
+		// another is the whole reprentation of the asset in json Format
 		err = ctx.GetStub().PutState(asset.ID, jsonAsset)
 
 		if err != nil {
